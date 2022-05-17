751ce9e406c5155690e3279a1bd888af93daea5c
Matthew Kight <70612402+mdkdoc15@users.noreply.github.com>
2022-05-17 11:06:45 -0400

    Player has lives

--- Space_Invaders/Assets/Scripts/PlayerHealth.cs
+++ Space_Invaders/Assets/Scripts/PlayerHealth.cs
@@ -1,18 +1,22 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class PlayerHealth : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    [SerializeField] private int lives = 3;
 
-    // Update is called once per frame
-    void Update()
+    private void OnCollisionEnter2D(Collision2D col)
     {
-        
+        if (col.gameObject.CompareTag("Enemy"))
+        {
+            Destroy(col.gameObject);
+            --lives;
+            if (lives < 0)
+            {
+                Destroy(gameObject);
+            }
+        }
     }
 }
